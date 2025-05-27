d25f92dedb44918eb17a9a5bde6bdc14a6ceb8d0
tejashwini-793 <tejashwini793@gmail.com>
2025-05-27 15:32:50 +0530

    message

---
+++ BankCustomer.java
@@ -0,0 +1,10 @@
+public class BankCustomer {
+    String custName, customerId, customerAddress;
+
+    public BankCustomer(String name, String id, String address) {
+        custName = name;
+        customerId = id;
+        customerAddress = address;
+    }
+}
+
